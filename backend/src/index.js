a4809e18a414fb531a7c9e368e0776f022bdddec
Mhmdabdulla <96muhammedabdulla@gmail.com>
2025-03-18 21:34:27 +0530

    Ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv'
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser'
 import { app,server } from './lib/socket.js';
+import path from 'path'
 dotenv.config()
 
 
@@ -21,8 +22,17 @@ app.use(cors({
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 server.listen(5001,()=>{
     console.log(`server running on ${PORT}`)
     connectDB()
