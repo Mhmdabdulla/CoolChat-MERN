0dde42cf59a5c9f0d269b9a6466eeb9adeba2611
Mhmdabdulla <96muhammedabdulla@gmail.com>
2025-03-18 20:50:59 +0530

    first commit before deployment

---
+++ backend/src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser'
+import { app,server } from './lib/socket.js';
+dotenv.config()
+
+
+
+app.use(express.json({ limit: '10mb' }));  // Set higher limit for Base64 images
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(cookieParser())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials:true
+}))
+
+app.use('/api/auth',authRoutes);
+app.use('/api/messages',messageRoutes);
+
+
+const PORT = process.env.PORT || 5001;
+server.listen(5001,()=>{
+    console.log(`server running on ${PORT}`)
+    connectDB()
+})
